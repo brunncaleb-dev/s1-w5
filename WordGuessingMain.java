a43ec9fb078a01cffcbbbb6a4705d8f72257fdf7
Tyler <tylerkypark@gmail.com>
2026-09-09 12:08:44 -0700

    h

--- WordGuessingMain.java
+++ WordGuessingMain.java
@@ -1,5 +1,15 @@
 public class WordGuessingMain{
     public static void main(String[] args){
-        
+        WordGuessing w = new WordGuessing("grape");
+        w.update("a");
+        System.out.println(w.getUserString());
+        w.update("r");
+        System.out.println(w.getUserString()); 
+        w.update("g");
+        System.out.println(w.getUserString());
+        w.update("p");
+        System.out.println(w.getUserString());
+        w.update("e");
+        System.out.println(w.getUserString());
     }
 }
